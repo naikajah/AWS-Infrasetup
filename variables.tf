d06942eccfce1d7c02bfd67bd042f809c4861ce9
naikajah <piyush.m82@gmail.com>
2020-08-04 22:42:50 +0100

    update code to the latest tf syntax

--- variables.tf
+++ variables.tf
@@ -3,17 +3,14 @@ variable "aws_region" {
 }
 
 variable "environment" {
-  type    = "string"
   default = "dev"
 }
 
 variable "vpc_cidr_blk" {
-  type    = "string"
   default = "10.162.0.0/16"
 }
 
 variable "subnet_cidr_blk" {
-  type    = "map"
   default = {
     "eu-west-1a" = "10.162.5.0/24,10.162.6.0/24"
     "eu-west-1b" = "10.162.7.0/24,10.162.8.0/24"
@@ -22,6 +19,5 @@ variable "subnet_cidr_blk" {
 }
 
 variable "layers_count" {
-  type    = "string"
   default = "2"
 }
